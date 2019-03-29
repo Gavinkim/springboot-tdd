b487a4f144ae67d16dcbe9a071e6ef6460c49899
GavinKim <dreamworld612@gmail.com>
2019-03-29 23:45:20 +0900

    added sample controller

---
+++ src/main/java/com/honghap/sample/SampleController.java
@@ -0,0 +1,16 @@
+package com.honghap.sample;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by gavinkim at 2019-03-29
+ */
+@RestController
+public class SampleController {
+
+    @GetMapping("/sample")
+    public String sample(){
+        return "welcome";
+    }
+}

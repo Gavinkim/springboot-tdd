c42812db9bdb1737ae3181535639c8ec9887dcb5
GavinKim <dreamworld612@gmail.com>
2019-03-30 00:14:29 +0900

    added sampleDTO and lombok

---
+++ src/main/java/com/honghap/sample/SampleDTO.java
@@ -0,0 +1,22 @@
+package com.honghap.sample;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * Created by gavinkim at 2019-03-30
+ */
+@Getter
+@Setter
+@ToString
+public class SampleDTO {
+
+    private String message;
+
+    @Builder
+    public SampleDTO(String message) {
+        this.message = message;
+    }
+}

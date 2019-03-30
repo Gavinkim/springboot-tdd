fa056f801545e9ea54f437c6644763cbedc4fdb5
GavinKim <dreamworld612@gmail.com>
2019-03-30 19:27:56 +0900

    change main class name and integration test about TodoController

--- src/test/java/com/honghap/sample/integration/TodoControllerTest.java
+++ src/test/java/com/honghap/sample/integration/TodoControllerTest.java
@@ -1,9 +1,9 @@
 package com.honghap.sample.integration;
 
 import com.honghap.sample.Application;
-import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
@@ -11,15 +11,12 @@ import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.hamcrest.Matchers.containsString;
-import static org.junit.Assert.assertThat;
-
 /**
  * Created by gavinkim at 2019-03-29
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class SampleControllerTest {
+public class TodoControllerTest {
 
     private static final String LOCAL_HOST = "http://localhost";
 
@@ -30,26 +27,16 @@ public class SampleControllerTest {
     private TestRestTemplate template;
 
     @Test
-    public void sample() {
-
-        ResponseEntity<String> response = template.getForEntity(createUrl("/sample"),String.class);
-
-        assertThat(response.getBody(), Matchers.equalTo("welcome"));
-    }
-
-
-    @Test
-    public void getSampleDto() throws Exception{
-
-        ResponseEntity<String> response = template.getForEntity(createUrl("/sample/dto"),String.class);
-
-        assertThat(response.getBody(), containsString("welcome"));
-    }
-
-    @Test
-    public void sampleWithParameter() throws Exception {
-        ResponseEntity<String> response = template.getForEntity(createUrl("/sample-with-parameter/gavin"),String.class);
-        assertThat(response.getBody(),containsString("welcome, gavin!"));
+    public void retrieveTodo() throws Exception {
+        String expected = "{\n" +
+                "id: 4,\n" +
+                "user: \"James\",\n" +
+                "desc: \"Study Django\",\n" +
+                "targetDate: \"2019-04-02\",\n" +
+                "done: false\n" +
+                "}";
+        ResponseEntity<String> response = template.getForEntity(createUrl("/users/james/todos/4"),String.class);
+        JSONAssert.assertEquals(expected,response.getBody(),false);
     }
 
     private String createUrl(String url) {
